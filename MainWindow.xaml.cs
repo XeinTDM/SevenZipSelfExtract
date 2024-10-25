80d88945e61a72b825f1ee4562e162dc3501537b
Xein <100153223+XeinTDM@users.noreply.github.com>
2024-10-25 22:45:06 +0200

    .

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using System.Windows;
+
+namespace SevenZipSelfExtract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                PathTextBox.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void Drag(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void WindowControlButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
